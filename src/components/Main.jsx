c793a3b9e2fd21d10e65c4e42bf7e022efe95f90
jmei403 <jason.mei403@gmail.com>
2024-09-03 15:20:02 -0400

    add components and dummy data for app

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <>
+      <img src="mars.png" alt="mars-demo-picture"></img>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <h2>The Brutal Martian Landscape</h2>
+      <div>
+        <p>Description</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis expedita vel autem cum fugiat earum consequuntur quaerat. Sed atque cumque officiis aspernatur debitis, magnam, maxime
+          blanditiis, laboriosam in error magni!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
