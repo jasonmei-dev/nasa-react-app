cf19b1509e6ef47f88a320bd25c3e7d9a51026d6
jmei403 <jason.mei403@gmail.com>
2024-09-03 16:05:15 -0400

    Footer styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
       <Main />
-      <Sidebar />
+      {showModal && <Sidebar />}
       <Footer />
     </>
   );
